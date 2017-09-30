c763193bb06fa130436b94575802a5d1c8693ff7
Bryce Collins <brycecollins0@gmail.com>
2017-09-29 22:43:01 -0700

    Attempting to make assets loadable without being listed in scripts. Can currently load them but do not have them as mutable.

--- RPG Clicker/Assets/Scripts/EnemyArea.cs
+++ RPG Clicker/Assets/Scripts/EnemyArea.cs
@@ -6,11 +6,20 @@ public class EnemyArea : MonoBehaviour {
 
 	private List<GameObject> enemies;
 
+	private GameObject basicEnemy1;
+	private string path = "Enemies/Basic Enemy 1";
 	// TODO
 	//private List<Missiles> missiles;
 
 	// Use this for initialization
 	void Start () {
+		//basicEnemy1 = Instantiate (Resources.Load (path, typeof(GameObject))) as GameObject;
+		basicEnemy1 = Resources.Load(path, typeof(GameObject)) as GameObject;
+		Instantiate (basicEnemy1);
+
+		//addEnemy (basicEnemy1, 1f, 1f);
+		//Debug.Log ("Enemy Field position is: " + transform.position.ToString ());
+		//Debug.Log ("BasicEnemy position is: + " + enemies[0].transform.position.ToString()); 
 
 	}
 	
@@ -25,7 +34,8 @@ public class EnemyArea : MonoBehaviour {
 	}
 
 	public bool enemiesLeft(){
-		if(enemies.
+		if (enemies.Count > 0) { return true;}
+		return false;
 	}
 
 }
