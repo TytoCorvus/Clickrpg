6a83b19bbdc65fba77eef356a70aa982a122b404
Bryce Collins <brycecollins0@gmail.com>
2017-10-06 00:13:26 -0700

    Today's work. Current prefab is acting up... hmmm...

---
+++ RPG Clicker/Assets/Scripts/BarScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarScript : MonoBehaviour {
+
+	[SerializeField] public Vector4 color;
+	[SerializeField] public bool isSolidColor;
+	[SerializeField] public float startRatio;
+	
+	private float testTime = 1f;
+	
+	private Image currentBar;
+	private Image flashingBar;
+	private float currentRatio;
+	
+	
+	
+	// Use this for initialization
+	void Start () {
+		Transform bar = transform.Find("CurrentBar");
+		if(bar == null){ Debug.Log("Bar is null");}
+		/*
+		currentBar = bar.GetComponent<Image>();
+		
+		if(startRatio != null){
+			currentRatio = startRatio;
+		}
+		else{ currentRatio = 1f;}
+		
+		if(isSolidColor){currentBar.color = color;}
+		*/
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//currentBar.transform.localScale = new Vector3(currentRatio, 1f, 1f);
+		//UpdateColor();
+		testTime -= Time.deltaTime;
+		if(testTime < 0 ){
+			Transform bar = transform.Find("CurrentBar");
+			if(bar == null){ Debug.Log("Bar is null");}
+			else{ Debug.Log("Bar exists");}
+		}
+		
+	}
+	
+	/*
+	public void SetRatio(float ratio){
+		currentRatio = ratio;
+		
+	}
+	
+	public void UpdateColor(){
+		if(isSolidColor){
+			if(currentRatio > .6f){
+				currentBar.color = new Vector4(0.0f, currentRatio, 0.0f, 1.0f);
+			}
+			else if(currentRatio > .3f){
+				currentBar.color = new Vector4(1f - (3 * currentRatio), currentRatio * currentRatio, 0.0f, 1.0f);
+			}
+			else{
+				currentBar.color = new Vector4(1.0f, 0f, 0f, 1f);
+			}
+		}
+	}
+	*/
+}
+
--- RPG Clicker/Assets/Scripts/BasicEnemy.cs
+++ RPG Clicker/Assets/Scripts/BasicEnemy.cs
@@ -6,20 +6,21 @@ using UnityEngine.UI;
 public class BasicEnemy : Enemy {
 
 	private static float defaultTimeBetweenAttacks = 4f;
-	[SerializeField] public HealthBarScript health;
-	private Transform healthBar;
+	public BarScript health;
+	public BarScript attackTimer;
+
 	private float damage = 4;
-	//[SerializeField] private double maxHP = 100;
+	[SerializeField] private double maxHP = 100;
 
 
 	// Use this for initialization
 	void Start () {
 		currentHP = 100;
-		maxHP = 100;
 		this.setTimeBetweenAttacks(defaultTimeBetweenAttacks);
 		timeSinceLastAttack = 0f;
 		
-		health = transform.GetChild(0).GetChild(0).gameObject.GetComponent<HealthBarScript>();
+		health = transform.GetChild(0).GetChild(0).gameObject.GetComponent<BarScript>();
+		attackTimer = transform.GetChild(0).GetChild(1).gameObject.GetComponent<BarScript>();
 	}
 	
 	// Update is called once per frame
@@ -34,7 +35,7 @@ public class BasicEnemy : Enemy {
 			Destroy (gameObject, 0f);
 		}
 		
-		health.SetHealthRatio((float)(currentHP/maxHP));
+		//health.SetRatio((float)(currentHP/maxHP));
 	}
 		
 	override public void Attack(){
